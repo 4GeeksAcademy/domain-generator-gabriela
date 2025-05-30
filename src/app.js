418f5fcb26902d31314bacac24e00c1aff33e94e
gabrielafariac <126210298+gabrielafariac@users.noreply.github.com>
2025-05-30 23:28:27 +0000

    last

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extensions = ['.com','.net','.us','.dog'];
+
+ 
+ for (let elm of pronoun){
+    for (let elm1 of adj){
+      for (let elm2 of noun){
+        for (let elm3 of extensions){
+          let result = elm + elm1 + elm2 + elm3;
+          console.log(result);
+          document.querySelector('#DomainName').innerHTML += "<li>" + result + "</li>";
+        }
+      }
+    }
+  }
+
+
+ 
+  
+
 };
